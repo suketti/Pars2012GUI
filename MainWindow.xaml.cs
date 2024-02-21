8f7433028778c8e836187f6552d19f5223173e67
sketchy <54027485+suketti@users.noreply.github.com>
2024-02-21 22:49:20 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+
+namespace Pars2012GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Versenyzo> Versenyzok { get; set; } = new ObservableCollection<Versenyzo>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                foreach (string line in File.ReadAllLines("./Selejtezo2012.txt").Skip(1))
+                {
+                    Versenyzok.Add(new Versenyzo(line));
+                }
+
+                cbVersenyzo.ItemsSource = Versenyzok;
+                cbVersenyzo.DisplayMemberPath = "Név";
+
+               cbVersenyzo.SelectedIndex = Versenyzok.IndexOf(Versenyzok.Where(v => v.Név == "Pars Krisztián").FirstOrDefault() ?? throw new Exception("Hibás alapadatok"));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Window_Loaded", MessageBoxButton.OK, MessageBoxImage.Error);   
+            }
+        }
+
+        private void VersenyzoCB_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            try
+            {
+                Versenyzo selectedVersenyzo = (Versenyzo)cbVersenyzo.SelectedItem;
+                LoadVersenyzoAdatai(selectedVersenyzo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nem sikerult betolteni");
+            }
+        }
+
+        private void LoadVersenyzoAdatai(Versenyzo versenyzo)
+        {
+            try
+            {
+                lblVersenyzoNev.Content = $"Név: {versenyzo.Név}";
+                lblVersenyzoCsoport.Content = $"Csoport: {versenyzo.Csoport}";
+                lblVersenyzoNemzet.Content = $"Nemzet: {versenyzo.Nemzet2}";
+                lblVersenyzoNemzetKod.Content = $"Kód: {versenyzo.Kód}";
+                lblVersenyzoSorozat.Content = $"Sorozat: {versenyzo.Sorozat}";
+                lblVersenyzoEredmeny.Content = $"Eredmény: {versenyzo.Eredmény2()}";
+
+                // Load Versenyzo Image
+                imgVersenyzoZaszlo.Source = new BitmapImage(new Uri($"./Images/{versenyzo.Kód}.png", UriKind.Relative));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "LoadVersenyzoAdatai", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Versenyzo.cs
@@ -0,0 +1,71 @@
+﻿namespace Pars2012GUI
+{
+    public class Versenyzo
+    {
+        //private String név;
+        float[] dobasok = new float[3];
+
+        public Versenyzo(String CSVsor)
+        {
+            string[] mezok = CSVsor.Split(';');
+            //this.név = mezok[0];
+            Név = mezok[0];
+            Csoport = mezok[1][0]; //A szó első karaktere.
+            NemzetÉsKód = mezok[2];
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (mezok[3 + i][0] == 'X')
+                {
+                    dobasok[i] = -1;
+                }
+                else if (mezok[3 + i][0] == '-')
+                {
+                    dobasok[i] = -2;
+                }
+                else
+                {
+                    dobasok[i] = float.Parse(mezok[3 + i]);
+                }
+            }
+        }
+        public String Név { get; set; }
+        public char Csoport { get; set; }
+        public String NemzetÉsKód { get; set; }
+        public float D1 { get => this.dobasok[0]; }
+        public float D2 { get => this.dobasok[1]; }
+        public float D3 { get => this.dobasok[2]; }
+        //7. Készítsen a Versenyző osztályban kódtagot (függvényt, jellemzőt stb.) az eredmény meghatározására Eredmény azonosítóval! A kódtag a versenyző legnagyobb dobásának értékét adja vissza!(Ha nem volt érvényes dobása a versenyzőnek, akkor ez - 1.0 lesz.)
+        public float Eredmény()
+        {
+            return Math.Max(Math.Max(this.D1, this.D2), this.D3);
+        }
+
+        public float Eredmény2()
+        {
+            return dobasok.Max();
+        }
+
+        public float EredmenyProp => dobasok.Max();
+
+
+        //8. Készítsen két kódtagot a Versenyző osztályban a nemzet és a nemzet hárombetűs kódjának (rövidítésének) meghatározására Nemzet és Kód azonosítókkal! Ügyeljen rá, hogy a nemzet neve több szóból is állhat! A Kód a nemzet hárombetűs kódjával zárójelek nélkül térjen vissza!
+
+        public String Nemzet()
+        {
+            int holVan = this.NemzetÉsKód.IndexOf('(');
+            return this.NemzetÉsKód.Substring(0, holVan - 1);
+        }
+
+        public String Nemzet2 => this.NemzetÉsKód.Substring(0, this.NemzetÉsKód.IndexOf('(') - 1);
+
+        public String Kód => this.NemzetÉsKód.Substring(this.NemzetÉsKód.IndexOf('(') + 1, 3);
+
+        public String Sorozat => $"{D1};{D2};{D3}";
+
+        public override string ToString()
+        {
+            return $"{Név};{Csoport};{NemzetÉsKód};{D1};{D2};{D3}";
+        }
+    }
+}
